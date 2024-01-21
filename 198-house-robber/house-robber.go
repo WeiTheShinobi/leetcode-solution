1906e320d1d5afb336ab16a84f22cd9086a545e1
WeiTheShinobi <weitheshinobi@gmail.com>
2024-01-21 12:10:50 +0800

    Time: 0 ms (100.00%) | Memory: 2.1 MB (68.84%) - LeetSync

---
+++ 198-house-robber/house-robber.go
@@ -0,0 +1,15 @@
+func rob(nums []int) int {
+    dp := make([]int, len(nums)+2)
+    
+    for i := 0; i < len(nums); i++ {
+        dp[i+2] = max(dp[i+1], dp[i]+nums[i])
+    }
+    return dp[len(dp)-1]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
