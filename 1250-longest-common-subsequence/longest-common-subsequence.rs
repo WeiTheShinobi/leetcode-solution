485ba6a31827ab65871d81b82f171d290aa39e00
WeiTheShinobi <weitheshinobi@gmail.com>
2024-01-25 10:43:22 +0800

    Time: 1790 ms (5.94%) | Memory: 6 MB (50.50%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.rs
+++ 1250-longest-common-subsequence/longest-common-subsequence.rs
@@ -2,14 +2,14 @@ use std::cmp::{max, min};
 
 impl Solution {
     pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
-        let mut dp = vec![vec![0;text2.len()+1];text1.len()+1];
+        let mut dp = vec![vec![0; text2.len() + 1]; text1.len() + 1];
 
-        for i in 0..text1.len() {
-            for j in 0..text2.len() {
+        for (i, _) in text1.chars().enumerate() {
+            for (j, _) in text2.chars().enumerate() {
                 if text1.chars().nth(i) == text2.chars().nth(j) {
-                    dp[i+1][j+1] = dp[i][j] + 1;
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
                 } else {
-                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
+                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
                 }
             }
         }
