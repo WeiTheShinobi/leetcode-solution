2111108ee6910ff5b49480029a4326bb4f2cd9ac
WeiTheShinobi <weitheshinobi@gmail.com>
2024-01-25 10:44:59 +0800

    Time: 4 ms (91.09%) | Memory: 6.1 MB (29.70%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.rs
+++ 1250-longest-common-subsequence/longest-common-subsequence.rs
@@ -4,9 +4,9 @@ impl Solution {
     pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
         let mut dp = vec![vec![0; text2.len() + 1]; text1.len() + 1];
 
-        for (i, _) in text1.chars().enumerate() {
-            for (j, _) in text2.chars().enumerate() {
-                if text1.chars().nth(i) == text2.chars().nth(j) {
+        for (i, x) in text1.chars().enumerate() {
+            for (j, y) in text2.chars().enumerate() {
+                if x == y {
                     dp[i + 1][j + 1] = dp[i][j] + 1;
                 } else {
                     dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
