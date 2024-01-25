d60bd8763fd93d37e11fbe45de5272df607a4387
WeiTheShinobi <weitheshinobi@gmail.com>
2024-01-25 10:34:16 +0800

    Time: 0 ms (100.00%) | Memory: 2.1 MB (99.46%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.go
+++ 1250-longest-common-subsequence/longest-common-subsequence.go
@@ -1,19 +1,37 @@
 func longestCommonSubsequence(text1 string, text2 string) int {
 	n, m := len(text1), len(text2)
 
-	dp := make([][]int, n+1)
-	for i := 0; i < n+1; i++ {
-		dp[i] = make([]int, m+1)
-	}
+	dp := make([]int, m+1)
 
 	for i := 0; i < n; i++ {
+		pre := 0
 		for j := 0; j < m; j++ {
 			if text1[i] == text2[j] {
-				dp[i+1][j+1] = dp[i][j] + 1
+				dp[j+1], pre = pre + 1, dp[j+1]
 			} else {
-				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+        pre = dp[j+1]
+				dp[j+1] = max(dp[j+1], dp[j])
 			}
 		}
 	}
-	return dp[n][m]
+
+	return dp[m]
+
+	//n, m := len(text1), len(text2)
+	//
+	//dp := make([][]int, n+1)
+	//for i := 0; i < n+1; i++ {
+	//	dp[i] = make([]int, m+1)
+	//}
+	//
+	//for i := 0; i < n+1; i++ {
+	//	for j := 0; j < m+1; j++ {
+	//		if text1[i] == text2[j] {
+	//			dp[i+1][j+1] = dp[i][j] + 1
+	//		} else {
+	//			dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+	//		}
+	//	}
+	//}
+	//return dp[n][m]
 }
