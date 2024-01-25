5457667bc3619bb73ec459005d78b512a420cf61
WeiTheShinobi <weitheshinobi@gmail.com>
2024-01-25 10:18:23 +0800

    Time: 8 ms (77.50%) | Memory: 14.6 MB (66.25%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.go
@@ -0,0 +1,19 @@
+func longestCommonSubsequence(text1 string, text2 string) int {
+	n, m := len(text1), len(text2)
+
+	dp := make([][]int, n+1)
+	for i := 0; i < n+1; i++ {
+		dp[i] = make([]int, m+1)
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if text1[i] == text2[j] {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+			}
+		}
+	}
+	return dp[n][m]
+}
