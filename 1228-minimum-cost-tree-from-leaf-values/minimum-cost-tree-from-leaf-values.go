8d062927045280b4bb4a499dc438b631f098e814
WeiTheShinobi <weitheshinobi@gmail.com>
2024-01-27 16:58:35 +0800

    Time: 2 ms (100.00%) | Memory: 2.1 MB (100.00%) - LeetSync

---
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.go
@@ -0,0 +1,23 @@
+func mctFromLeafValues(arr []int) int {
+	var stack []int
+	result := 0
+	for _, x := range arr {
+		for len(stack) > 0 && stack[len(stack)-1] <= x {
+			if len(stack) == 1 || stack[len(stack)-2] > x {
+				result += stack[len(stack)-1] * x
+			} else {
+				result += stack[len(stack)-1] * stack[len(stack)-2]
+			}
+			// [x, y] z  | y<z || x<z -> x*y
+			// [x, z] 
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, x)
+	}
+	for len(stack) > 1 {
+		result += stack[len(stack)-1]*stack[len(stack)-2]
+		stack = stack[:len(stack)-1]
+	}
+
+	return result
+}
