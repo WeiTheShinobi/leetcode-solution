cc1bff10d93b27aeb645ad2f28b2efea3dc2f4df
WeiTheShinobi <weitheshinobi@gmail.com>
2024-01-31 09:40:33 +0800

    Time: 38 ms (99.31%) | Memory: 4 MB (78.62%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.rs
+++ 739-daily-temperatures/daily-temperatures.rs
@@ -3,18 +3,19 @@ impl Solution {
         let mut result = vec![0; temperatures.len()];
         let mut stk = vec![];
 
-        for (i, &t) in temperatures.iter().enumerate() {
-            let i = i as i32;
-            if stk.is_empty() {
-                stk.push(i);
-            } else {
-                while !stk.is_empty() && temperatures[stk[stk.len() - 1] as usize] < t {
-                    let x = stk.pop().unwrap();
-                    result[x as usize] = i - x;
+        temperatures.iter().enumerate().
+            for_each(|(i, &t)| {
+                let i = i as i32;
+                if stk.is_empty() {
+                    stk.push(i);
+                } else {
+                    while !stk.is_empty() && temperatures[stk[stk.len() - 1] as usize] < t {
+                        let x = stk.pop().unwrap();
+                        result[x as usize] = i - x;
+                    }
+                    stk.push(i);
                 }
-                stk.push(i);
-            }
-        }
+            });
 
         result
     }
