55d880227fd579b432e01777eabd2e08cd85d06f
WeiTheShinobi <weitheshinobi@gmail.com>
2024-05-10 12:17:39 +0800

    Time: 134 ms (100.00%) | Memory: 11.8 MB (100.00%) - LeetSync

---
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn kth_smallest_prime_fraction(arr: Vec<i32>, k: i32) -> Vec<i32> {
+        struct Pair {
+            x: i32,
+            y: i32,
+            r: f32,
+        }
+
+        let mut r_arr = vec![];
+        for i in 0..arr.len() {
+            for j in i..arr.len() {
+                let r = arr[i] as f32 / arr[j] as f32;
+                r_arr.push(Pair {
+                    x: arr[i],
+                    y: arr[j],
+                    r,
+                });
+            }
+        }
+
+        r_arr.sort_by(|a, b| a.r.partial_cmp(&b.r).unwrap());
+
+        vec![r_arr[(k-1) as usize].x, r_arr[(k-1) as usize].y]
+    }
+}
