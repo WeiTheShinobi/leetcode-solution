13757d31f11082b71f8c0aa99288a925b10cecba
Kao-Wei Yeh <weitheshinobi@gmail.com>
2024-05-31 13:17:09 +0800

    Time: 1 ms (77.78%) | Memory: 2.3 MB (77.78%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> Vec<i32> {
+        let mut xor = 0;
+        nums.iter().for_each(|&x| xor ^= x);
+        
+        let right_most = xor & (-xor);
+        nums.iter().fold(vec![0,0], |mut acc, x| {
+            if x & right_most > 0 {
+                acc[0] ^= x;
+                acc
+            } else {
+                acc[1] ^= x;
+                acc
+            }
+        })
+    }
+}
