5ddb49dce3feae2cae34de1ff17bee33886d6912
Kao-Wei Yeh <weitheshinobi@gmail.com>
2024-06-04 01:48:06 +0800

    Time: 0 ms (100.00%) | Memory: 2.2 MB (33.33%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.rs
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.rs
@@ -2,9 +2,8 @@ impl Solution {
     pub fn append_characters(s: String, t: String) -> i32 {
         (t.len() - s.
             bytes().
-            zip(vec![0; s.len()]).
             fold(0 , |acc, x| {
-                if acc < t.len() && x.0 == t.as_bytes()[acc] {
+                if acc < t.len() && x == t.as_bytes()[acc] {
                     acc + 1
                 } else {
                     acc
