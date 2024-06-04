5ef04ed89f87ea3cd9668c1af9deee010087d428
Kao-Wei Yeh <weitheshinobi@gmail.com>
2024-06-04 11:29:40 +0800

    Time: 1 ms (67.27%) | Memory: 2.2 MB (52.73%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.rs
+++ 409-longest-palindrome/longest-palindrome.rs
@@ -3,12 +3,12 @@ impl Solution {
         s.bytes().fold(vec![0; 58], |mut acc, x| {
             acc[(x - b'A') as usize] += 1;
             acc
-        }).iter().fold(0, |acc, &x| {
+        }).iter().fold(0, |acc, &x|
             acc + if acc & 1 == 0 {
                 x
             } else {
                 x - (x&1)
             }
-        })
+        )
     }
 }
