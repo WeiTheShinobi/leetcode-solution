aff1b3e1f7cbd35acfd5736c8b6804f27ff601cf
Kao-Wei Yeh <weitheshinobi@gmail.com>
2024-06-04 11:25:43 +0800

    Time: 1 ms (67.27%) | Memory: 2.2 MB (52.73%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn longest_palindrome(s: String) -> i32 {
+        s.bytes().fold(vec![0; 58], |mut acc, x| {
+            acc[(x - b'A') as usize] += 1;
+            acc
+        }).iter().fold(0, |acc, &x| {
+            acc + if acc & 1 == 0 {
+                x
+            } else {
+                x - (x&1)
+            }
+        })
+    }
+}
