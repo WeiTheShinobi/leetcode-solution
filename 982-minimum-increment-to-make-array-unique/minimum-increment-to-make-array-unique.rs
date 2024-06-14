24f57748e35f392ff0c18907dc4bca476196c475
Kao-Wei Yeh <weitheshinobi@gmail.com>
2024-06-14 13:01:19 +0800

    Time: 25 ms (50.00%) | Memory: 2.9 MB (75.00%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.rs
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.rs
@@ -3,12 +3,12 @@ impl Solution {
         nums.sort_unstable();
 
         (1..nums.len()).fold(0, |acc, idx| {
-            let val = nums[idx] - nums[idx - 1];
-            if val <= 0 {
-                nums[idx] = nums[idx-1] + 1;
-                acc + 1 - val
-            } else {
-                acc
+            match nums[idx] - nums[idx - 1] {
+                val if val <= 0 => {
+                    nums[idx] = nums[idx - 1] + 1;
+                    acc + 1 - val
+                }
+                _ => acc,
             }
         })
     }
