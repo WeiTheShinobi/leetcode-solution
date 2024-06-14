5f444f88adf51cb0df08d08abbaa105b19030562
Kao-Wei Yeh <weitheshinobi@gmail.com>
2024-06-14 12:42:16 +0800

    Time: 20 ms (100.00%) | Memory: 3 MB (50.00%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.rs
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.rs
@@ -1,28 +1,15 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn min_increment_for_unique(mut nums: Vec<i32>) -> i32 {
-        nums.sort();
-        let mut m = HashSet::new();
-        nums.iter().for_each(|&x| {
-            m.insert(x);
-        });
+        nums.sort_unstable();
 
-        let mut result = 0;
-        let mut i = -1;
-        for j in 1..nums.len() {
-            if nums[j] == nums[j-1] {
-                if i < nums[j] {
-                    i = nums[j] + 1;
-                }
-                while m.contains(&i) {
-                    i += 1;
-                }
-                result += i - nums[j];
-                m.insert(i);
+        (1..nums.len()).fold(0, |acc, idx| {
+            let val = nums[idx] - nums[idx - 1];
+            if val <= 0 {
+                nums[idx] = nums[idx-1] + 1;
+                acc + 1 - val
+            } else {
+                acc
             }
-        }
-
-        result
+        })
     }
 }
