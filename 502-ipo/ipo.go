d36b8499f05f3d8866dadaa99f0d69b14d8a139a
Kao-Wei Yeh <weitheshinobi@gmail.com>
2024-06-15 12:19:48 +0800

    Time: 136 ms (84.09%) | Memory: 11.1 MB (86.36%) - LeetSync

---
+++ 502-ipo/ipo.go
@@ -0,0 +1,34 @@
+func findMaximizedCapital(k int, w int, profits []int, capital []int) int {
+	n := len(profits)
+	type pair struct{ c, p int }
+	arr := make([]pair, n)
+	for i, p := range profits {
+		arr[i] = pair{capital[i], p}
+	}
+	sort.Slice(arr, func(i, j int) bool { return arr[i].c < arr[j].c })
+
+	h := &hp{}
+	for curr := 0; k > 0; k-- {
+		for curr < len(arr) && arr[curr].c <= w {
+			heap.Push(h, arr[curr].p)
+            curr++
+		}
+		if h.Len() == 0 {
+			break
+		}
+		w += heap.Pop(h).(int)
+	}
+
+	return w
+}
+
+type hp struct{ sort.IntSlice }
+
+func (h hp) Less(i, j int) bool  { return h.IntSlice[i] > h.IntSlice[j] }
+func (h *hp) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
