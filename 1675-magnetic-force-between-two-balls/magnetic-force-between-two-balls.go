a612140bb05d4c1d0a121c3d592cf8b7391ff1a7
Kao-Wei Yeh <weitheshinobi@gmail.com>
2024-06-20 10:04:48 +0800

    Time: 92 ms (100.00%) | Memory: 8.1 MB (84.21%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.go
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.go
@@ -12,15 +12,15 @@ func maxDistance(position []int, m int) int {
 	}
 
 	sort.Ints(position)
-	l, r, result := 0, position[len(position)-1]-position[0], 0
+	l, r, result := -1, position[len(position)-1]-position[0]+1, 0
 
-	for l <= r {
+	for l+1 < r {
 		step := (l + r) / 2
 		if check(step) {
-			l = step + 1
+			l = step
 			result = step
 		} else {
-			r = step - 1
+			r = step
 		}
 	}
 
